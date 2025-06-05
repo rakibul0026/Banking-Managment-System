875e56ee6473fd82b68d535929a7430ae1ee0a8c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-06-05 15:14:58 +0600

    Update BankingApplication.java

--- Bank Management system/BankingApplication.java
+++ Bank Management system/BankingApplication.java
@@ -1,1 +1,13 @@
+public class BankingApplication {
+    public static void main(String[] args) {
+        // Polymorphism: BankAccount reference can point to SavingsAccount
+        Account bankAccount = new BankAccount("Fouat", "1026");
+        bankAccount.showMenu();
 
+        // Uncomment below to test SavingsAccount with interest
+        /*
+        Account savings = new SavingsAccount("Fouat", "1026", 5.0);
+        savings.showMenu();
+        */
+    }
+}
